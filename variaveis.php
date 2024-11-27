47853cb3e9ad4def0761fef5b9fcd2c1d7cb262c
DavidDuarte0319 <davidsilva582@gmail.com>
2024-11-26 21:39:24 -0300

    variaveis php

---
+++ variaveis.php
@@ -0,0 +1,29 @@
+<?php
+
+// CONST sao sempre unicas
+const CPF = "123456789";
+const VELOCIDADE_DA_LUZ = "320K\S2";
+
+
+// tipos de dados primitivos do PHP.
+$inteiro = 2;
+$nomePessoa = "David"; // style guide Camel Case
+$nome_pessoa = "David"; // style guide Snake Case
+
+$string = "um texto qualquer" 
+$string = "joao do caminhao" // override = substituir o que existia
+
+$float = 2.5;
+$double = 2.5468;
+$char ='a';
+
+// tipos de dados estruturais
+$array = arrray ();
+$array_short = [];
+
+$objeto = new stdClass(); // POO - OOP
+$classe = new stdClass();
+
+
+
+

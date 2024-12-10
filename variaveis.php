cc61d961000ecc15b0cffd9be58294ed8128697a
DavidDuarte <davidslva582@gmail.com>
2024-12-10 19:34:21 -0300

    variaveis

--- variaveis.php
+++ variaveis.php
@@ -10,20 +10,61 @@ $inteiro = 2;
 $nomePessoa = "David"; // style guide Camel Case
 $nome_pessoa = "David"; // style guide Snake Case
 
-$string = "um texto qualquer" 
-$string = "joao do caminhao" // override = substituir o que existia
+$string = "um texto qualquer";
+$string = "joao do caminhao"; // override = substituir o que existia
 
 $float = 2.5;
 $double = 2.5468;
 $char ='a';
 
 // tipos de dados estruturais
-$array = arrray ();
+$array = array ();
 $array_short = [];
 
 $objeto = new stdClass(); // POO - OOP
 $classe = new stdClass();
 
 
+// POO - OOP
+class CanetaAzul {
+    // Atributos e/ou Propriedades
+    public const COR = "Azul";
+    public const PLASTICO = true;
+    public $nome;
+    public $tipoMaterial = "Plastico";
+    public $dimensoes;
+    public $tipo;
 
+   // metodos e/ou funcoes
+   public function escrever() {
+
+   }
+
+   public function pintar() {
+
+   }
+}
+
+echo "Ola 
+    <b>
+        <i>
+            mundo!
+        </i>
+    </b>
+<br>"; // <br></br> = <br/> ou <br>
+
+$string = "joao do caminhao";
+echo $string; // joao do caminhao
+
+echo "<br>CPF: " . CPF;
+
+$a = 10;
+$b = 20;
+$x =  $a + $b;
+echo "<br>x: " . $x;
+
+$soma =  10 + 10;
+echo "<br>soma: " . $soma;
+
+// Tema de casa sera fazer a tabuada (6, 7) e mostra na tela.
 

66fb7dc3470c6e27f4dd39da2ffb0010449d0eee
DavidDuarte <davidsilva82@gmail.com>
2024-12-17 22:02:27 -0300

    calendario

--- triangulos.php
+++ triangulos.php
@@ -5,19 +5,19 @@
  * diga qual tipo do triangulo:
  * 1 - equilatero 3 lados igual 
  * 2 - isosceles 2 lados iguais 
- * 3 - escaleno quando todos sao diferentes.
+ * 3 - escaleno quando todos os lados sao diferentes.
  */
 
- $ladoA = 5;
- $ladoB = 6;
+ $ladoA = 10;
+ $ladoB = 10;
  $ladoC = 7;
 
  if ($ladoA == $ladoB && $ladoB == $ladoC) {
     echo "As medias informadas forma um triangulo Equilatero";
  }
- if ($ladoA == $ladoB && $ladoB == $ladoA && $ladoC != $ladoA && $ladoB) {
+else  if ($ladoA == $ladoB || $ladoB == $ladoC || $ladoA == $ladoC) {
     echo "As medias informadas forma um triangulo Isosceles";
  }
- if ($ladoA != $ladoB && $ladoB != $ladoC) {
+   else if ($ladoA != $ladoB && $ladoB != $ladoC) {
     echo "As medias informadas forma um triangulo Escaleno";
  }
